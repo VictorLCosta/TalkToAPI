91195f25e255479089ac48868a22442d2e63da7b
VictorLCosta <victorlc2019@outlook.com>
2020-12-15 18:05:02 -0300

    Adding versioning and swagger

--- TalkToAPI/Startup.cs
+++ TalkToAPI/Startup.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
 using TalkToAPI.Database;
+using TalkToAPI.Helpers;
+using TalkToAPI.V1.Models;
+using TalkToAPI.V1.Repositories;
+using TalkToAPI.V1.Repositories.Contracts;
 
 namespace TalkToAPI
 {
@@ -24,15 +35,106 @@ namespace TalkToAPI
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApiBehaviorOptions>(op => {
+                op.SuppressModelStateInvalidFilter = true;
+            });
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<TalkToContext>()
+                .AddDefaultTokenProviders();
+
             services.AddDbContext<TalkToContext>(cfg => {
                 cfg.UseSqlite(Conf.GetConnectionString("TalkToContext"));
             });
 
-            services.AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
-                .AddJsonOptions(opt => {
-                    opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            services.AddMvc(config => {
+                config.ReturnHttpNotAcceptable = true;
+                config.InputFormatters.Add(new XmlSerializerInputFormatter(config));
+                config.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+            .AddJsonOptions(opt => {
+                opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+
+            #region Versioning Config
+
+            services.AddApiVersioning(op => {
+                op.ReportApiVersions = true;
+                op.DefaultApiVersion = new ApiVersion(1, 0);
+                op.AssumeDefaultVersionWhenUnspecified = true;
+            });
+
+            services.AddSwaggerGen(c => {
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme()
+                {
+                    In = "header",
+                    Type = "apiKey",
+                    Description = "Adicione o JSON Web Token (JWT) para autenticar",
+                    Name = "Authorization"
                 });
+
+                var security = new Dictionary<string, IEnumerable<string>>()
+                {
+                    { "Bearer", new string[] {} }
+                };
+
+                c.AddSecurityRequirement(security);
+
+
+                c.ResolveConflictingActions(apiDesc => apiDesc.First());
+
+                c.SwaggerDoc("v1.0", new Info { Title = "TalkToAPI - V1.0", Version = "v1.0" });
+
+                c.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    var actionApiVersionModel = apiDesc.ActionDescriptor?.GetApiVersion();
+                    if (actionApiVersionModel == null)
+                    {
+                        return true;
+                    }
+                    if (actionApiVersionModel.DeclaredApiVersions.Any())
+                    {
+                        return actionApiVersionModel.DeclaredApiVersions.Any(v => $"v{v.ToString()}" == docName);
+                    }
+                    return actionApiVersionModel.ImplementedApiVersions.Any(v => $"v{v.ToString()}" == docName);
+                });
+
+                c.OperationFilter<ApiVersionOperationFilter>();
+            });
+
+            #endregion
+
+            #region JWT Config
+
+            services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt => {
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Conf.GetValue<string>("Key")))
+                };
+            });
+
+            services.AddAuthorization(opt => {
+                opt.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                                            .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                                            .RequireAuthenticatedUser().Build()
+                );
+            });
+
+            #endregion
+
+            //REPOSITORIES
+            services.AddScoped<ITokenRepository, TokenRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -41,9 +143,21 @@ namespace TalkToAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
             app.UseStatusCodePages();
+            app.UseAuthentication();
+            app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "TalkToAPI - V1.0");
+                c.RoutePrefix = String.Empty;
+            });
         }
     }
 }
