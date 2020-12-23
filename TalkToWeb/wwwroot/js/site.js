411db2180b888a24e111b2279973e9f18762a1a2
VictorLCosta <victorlc2019@outlook.com>
2020-12-23 17:23:48 -0300

    Creating web project

---
+++ TalkToWeb/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿function TesteCors()
+{
+    var tokenJWT = ""
+    var service = ""
+    $("#result").html("Loading...")
+
+    $.ajax({
+        url: service,
+        method: "GET",
+        crossDomain: true,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer" + tokenJWT);
+        },
+        success: function (data, status, xhr) {
+            $("#result").html(data);
+        }
+    })
+}
